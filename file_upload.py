59b5f69f372f21df824b315b947041c2c30998fe
Christian <christianbarrios@ufm.edu>
2023-10-25 22:21:40 -0600

    modificacion de la creacion de carros

---
+++ file_upload.py
@@ -0,0 +1,5 @@
+# En file_upload.py
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
